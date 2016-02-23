e1ce6df916c086c7147395d5525cac8ba44614be
Breiden Busch <breiden.busch@gmail.com>
2016-02-23 12:30:56 -0700

    Finished Project Plus Some Extra Features

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,14 @@
 angular.module('timeApp').controller('mainCtrl', function($scope) {
 
   $scope.name = 'Breiden';
+  $scope.ctrlFormat = 'medium';
+
+  $scope.updateFormat = function() {
+    if ($scope.ctrlFormat === 'medium') {
+      $scope.ctrlFormat = 'short';
+    } else {
+      $scope.ctrlFormat = 'medium';
+    }
+  }
 
 })
--- js/timeDirective.js
+++ js/timeDirective.js
@@ -3,10 +3,22 @@ angular.module('timeApp').directive('showTime', function() {
   //this object represents the instructions for how our directive is supposed to run
   return {
     restrict: 'E',
-    template:'<div> The current time is {{time}} </div>',
+    template:'<div> The current time is {{time | date: format}} </div><div> The current time is {{gmtTime | date: format}} </div>',
+    scope: {
+      format: '=',
+      update: '&'
+    },
     link: function(scope, element, attrs) {
       var currentTime = new Date();
-      scope.time = currentTime.toString();
+      scope.gmtTime = currentTime.toGMTString();
+      scope.time = new Date(currentTime.toGMTString());
+      element.css({color: 'blue', fontWeight: 'bold'});
+      element.on('mousedown', function() {
+        // scope.time = new Date();
+        // scope.gmtTime = currentTime.toGMTString();
+        scope.update();
+        scope.$apply();
+      });
     }
   }
 
